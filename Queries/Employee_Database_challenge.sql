aba7a84a1c2b62fc9c36791d3ded2c90b865e6da
vflopez77 <68453606+vflopez77@users.noreply.github.com>
2020-09-03 17:24:15 -0400

    Module 7 Challenge Deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+-- Module 7 Challenge Deliverable 1
 -- Getting Retiring Employees by Title
 SELECT 	e.emp_no, 
 		e.first_name, 
@@ -12,15 +13,14 @@ ON (e.emp_no = ti.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
-
 -- Use Distinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (rt.emp_no) 
-rt.emp_no,
-rt.first_name,
-rt.last_name,
-rt.title
+		rt.emp_no,
+		rt.first_name,
+		rt.last_name,
+		rt.title
 INTO unique_titles
-FROM retirement_titles rt
+FROM retirement_titles as rt
 ORDER BY rt.emp_no, rt.from_date DESC;
 
 -- Getting Retiring Titles
@@ -29,4 +29,25 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
+
+-- Module 7 Challenge Deliverable 2
+-- Getting Mentorship Eligibility
+SELECT DISTINCT ON (e.emp_no)
+		e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		ti.title
+INTO mentorship_eligibility
+FROM employees as e
+LEFT JOIN dept_emp de 
+	ON (e.emp_no = de.emp_no)
+LEFT JOIN titles ti on 
+	(e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01') AND (ti.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no
+
 
