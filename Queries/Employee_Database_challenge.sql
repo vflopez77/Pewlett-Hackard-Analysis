f88dbe667c53052c2df91ab4941be197c06bb0c2
vflopez77 <68453606+vflopez77@users.noreply.github.com>
2020-09-05 22:25:39 -0400

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -8,7 +8,7 @@ SELECT 	e.emp_no,
 		ti.to_date
 INTO retirement_titles
 FROM employees as e
-LEFT JOIN titles as ti
+INNER JOIN titles as ti
 ON (e.emp_no = ti.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
@@ -42,9 +42,9 @@ SELECT DISTINCT ON (e.emp_no)
 		ti.title
 INTO mentorship_eligibility
 FROM employees as e
-LEFT JOIN dept_emp de 
+INNER JOIN dept_emp de 
 	ON (e.emp_no = de.emp_no)
-LEFT JOIN titles ti on 
+INNER JOIN titles ti on 
 	(e.emp_no = ti.emp_no)
 WHERE (de.to_date = '9999-01-01') AND (ti.to_date = '9999-01-01')
 	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
